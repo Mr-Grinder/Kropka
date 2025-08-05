26868b329b76b0efa48d467455a5201be7c17b85
Mr-Grinder <aaa3122556@gmail.com>
2025-08-05 22:09:44 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -4,6 +4,8 @@ import geoip2.database
 import user_agents
 import os
 from dotenv import load_dotenv
+import requests
+
 
 load_dotenv()
 
@@ -11,7 +13,17 @@ app = Flask(__name__)
 
 # Шлях до GeoLite2 бази
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MMDB_PATH = os.getenv("GEOIP_MMDB_PATH")
+MMDB_PATH = os.getenv("MMDB_PATH", "GeoLite2-City.mmdb")
+if not os.path.exists(MMDB_PATH):
+    print("GeoLite2-City.mmdb not found, downloading...")
+    mmdb_url = os.getenv("MMDB_URL")
+    if not mmdb_url:
+        raise RuntimeError("MMDB_URL not set in environment variables")
+    r = requests.get(mmdb_url)
+    with open(MMDB_PATH, "wb") as f:
+        f.write(r.content)
+    print("Downloaded GeoLite2-City.mmdb")
+
 geoip_reader = geoip2.database.Reader(MMDB_PATH)
 
 @app.route("/store/apps/details")
